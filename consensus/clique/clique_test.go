c89bc6863561134da381b275d782fe6c95142b19
Jakub Pajek <j.pajek@freebit.net>
2021-11-09 16:22:04 +0900

    Clique modification: separated signers into voters and signers

--- consensus/clique/clique_test.go
+++ consensus/clique/clique_test.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// TODOJAKUB add support for new permission model testing (voter/signer)
+
 package clique
 
 import (
