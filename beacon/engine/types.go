b450601a3bb769c99c03dd3ce7befdb86c3edb85
Jakub Pajek <j.pajek@freebit.net>
2022-05-23 15:23:39 +0900

    core: changed extra data size to 0 from 32

--- beacon/engine/types.go
+++ beacon/engine/types.go
@@ -164,6 +164,8 @@ func ExecutableDataToBlock(params ExecutableData) (*types.Block, error) {
 	if err != nil {
 		return nil, err
 	}
+	// MEMO by Jakub Pajek (zero size extra)
+	// How does the below check affect zero size extra data in TChain?
 	if len(params.ExtraData) > 32 {
 		return nil, fmt.Errorf("invalid extradata length: %v", len(params.ExtraData))
 	}
