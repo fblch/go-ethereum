c9c4c09a233fefc03e0443f46ebf03a59c139791
Seungbae Yu <dbadoy4874@gmail.com>
2023-07-14 21:58:27 +0900

    cmd/bootnode, p2p: support for alternate mapped ports (#26359)
    This changes the port mapping procedure such that, when the requested port is unavailable
    an alternative port suggested by the router is used instead.
    
    We now also repeatedly request the external IP from the router in order to catch any IP changes.
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- p2p/nat/natupnp.go
+++ p2p/nat/natupnp.go
@@ -19,6 +19,8 @@ package nat
 import (
 	"errors"
 	"fmt"
+	"math"
+	"math/rand"
 	"net"
 	"strings"
 	"sync"
@@ -42,6 +44,7 @@ type upnp struct {
 	client      upnpClient
 	mu          sync.Mutex
 	lastReqTime time.Time
+	rand        *rand.Rand
 }
 
 type upnpClient interface {
@@ -78,20 +81,20 @@ func (n *upnp) ExternalIP() (addr net.IP, err error) {
 	return ip, nil
 }
 
-func (n *upnp) AddMapping(protocol string, extport, intport int, desc string, lifetime time.Duration) error {
+func (n *upnp) AddMapping(protocol string, extport, intport int, desc string, lifetime time.Duration) (uint16, error) {
 	// MODIFIED by Jakub Pajek BEG (x/mobile: Calling net.Interfaces() fails on Android SDK 30+)
 	/*
 		ip, err := n.internalAddress()
 		if err != nil {
-			return nil // TODO: Shouldn't we return the error?
+			return 0, nil // TODO: Shouldn't we return the error?
 		}
 	*/
 	var ip net.IP
+	var err error
 	if n.local == nil {
-		var err error
 		ip, err = n.internalAddress(false)
 		if err != nil {
-			return nil // TODO: Shouldn't we return the error?
+			return 0, nil // TODO: Shouldn't we return the error?
 		}
 	} else {
 		ip = n.local
@@ -101,9 +104,41 @@ func (n *upnp) AddMapping(protocol string, extport, intport int, desc string, li
 	lifetimeS := uint32(lifetime / time.Second)
 	n.DeleteMapping(protocol, extport, intport)
 
-	return n.withRateLimit(func() error {
+	err = n.withRateLimit(func() error {
 		return n.client.AddPortMapping("", uint16(extport), protocol, uint16(intport), ip.String(), true, desc, lifetimeS)
 	})
+	if err == nil {
+		return uint16(extport), nil
+	}
+
+	return uint16(extport), n.withRateLimit(func() error {
+		p, err := n.addAnyPortMapping(protocol, extport, intport, ip, desc, lifetimeS)
+		if err == nil {
+			extport = int(p)
+		}
+		return err
+	})
+}
+
+func (n *upnp) addAnyPortMapping(protocol string, extport, intport int, ip net.IP, desc string, lifetimeS uint32) (uint16, error) {
+	if client, ok := n.client.(*internetgateway2.WANIPConnection2); ok {
+		return client.AddAnyPortMapping("", uint16(extport), protocol, uint16(intport), ip.String(), true, desc, lifetimeS)
+	}
+	// It will retry with a random port number if the client does
+	// not support AddAnyPortMapping.
+	extport = n.randomPort()
+	err := n.client.AddPortMapping("", uint16(extport), protocol, uint16(intport), ip.String(), true, desc, lifetimeS)
+	if err != nil {
+		return 0, err
+	}
+	return uint16(extport), nil
+}
+
+func (n *upnp) randomPort() int {
+	if n.rand == nil {
+		n.rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	}
+	return n.rand.Intn(math.MaxUint16-10000) + 10000
 }
 
 // MODIFIED by Jakub Pajek (x/mobile: Calling net.Interfaces() fails on Android SDK 30+)
