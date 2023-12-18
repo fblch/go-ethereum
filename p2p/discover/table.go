58bae49c7f56c548bb05a66745705f0582a90e1d
Felix Lange <fjl@twurst.com>
2023-12-18 10:47:21 +0100

    p2p/discover: add liveness check in collectTableNodes (#28686)
    * p2p/discover: add liveness check in collectTableNodes
    
    * p2p/discover: fix test
    
    * p2p/discover: rename to appendLiveNodes
    
    * p2p/discover: add dedup logic back
    
    * p2p/discover: simplify
    
    * p2p/discover: fix issue found by test

--- p2p/discover/table.go
+++ p2p/discover/table.go
@@ -462,6 +462,32 @@ func (tab *Table) findnodeByID(target enode.ID, nresults int, preferLive bool) *
 	return nodes
 }
 
+// appendLiveNodes adds nodes at the given distance to the result slice.
+func (tab *Table) appendLiveNodes(dist uint, result []*enode.Node) []*enode.Node {
+	if dist > 256 {
+		return result
+	}
+	if dist == 0 {
+		return append(result, tab.self())
+	}
+
+	tab.mutex.Lock()
+	defer tab.mutex.Unlock()
+	for _, n := range tab.bucketAtDistance(int(dist)).entries {
+		// MODIFIED by Jakub Pajek (mobile connectivity)
+		// WAN bootnodes can not correctly perform a liveness check of mobile nodes
+		// (Private addresses behind CGNAT are always unreachable from the Internet).
+		// Unless we explicitly allow unverified mobile nodes here, they will never
+		// be advertised.
+		//if n.livenessChecks >= 1 {
+		if n.livenessChecks >= 1 || netutil.IsMobileLAN(n.IP()) {
+			node := n.Node // avoid handing out pointer to struct field
+			result = append(result, &node)
+		}
+	}
+	return result
+}
+
 // len returns the number of nodes in the table.
 func (tab *Table) len() (n int) {
 	tab.mutex.Lock()
