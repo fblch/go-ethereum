bf1abdc67beb77dbba49f90ace07cfb9ecdb0f31
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-03-05 18:46:46 +0900

    for debug: increased maxBurstSize to 8192.

--- p2p/elstack/vpn_delegate.go
+++ p2p/elstack/vpn_delegate.go
@@ -135,7 +135,7 @@ func SetupEL(cfg *ELConfig, results chan LinkedResult, quit <-chan struct{}) {
 	// tcpBuffSize := uint64(65536)
 	// udpBuffSize := uint64(65536)
 	// udpMetaSize := uint64(32)
-	maxBurstSize := uint64(4096)
+	maxBurstSize := uint64(8192)
 	// tcpBuffSize := uint64(65536)
 	// udpBuffSize := uint64(65536)
 	// udpMetaSize := uint64(2048)
