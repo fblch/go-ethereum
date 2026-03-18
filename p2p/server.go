1f664e20ddd052f4b8f3d5c2170604c284bc2b46
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-03-18 10:52:13 +0900

    p2p: align EL startup failure comment with actual behavior

--- p2p/server.go
+++ p2p/server.go
@@ -439,7 +439,7 @@ func (srv *Server) Start() (err error) {
 	// ADDED by Hinata AWAIISHIMA
 	if srv.EL != nil && srv.EL.Use {
 		if err := srv.setupEL(); err != nil {
-			// when EL stack is unavailable, output a log and setting p2p with default connections.
+			// If EL setup fails, log the reason and abort startup.
 			srv.log.Error("EL setup failed", "err", err)
 			return err
 		}
