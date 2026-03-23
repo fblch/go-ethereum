cf0b66656bca7c429ef571838162a9e2f236a224
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-03-23 17:33:09 +0900

    p2p/elstack: Deleted wrapper type, ElStackUdpConn

--- p2p/elstack/udp.go
+++ p2p/elstack/udp.go
@@ -7,15 +7,7 @@ import (
 	"github.com/ethereum/go-ethereum/p2p/elstack/el_stack"
 )
 
-type ElStackUdpConn struct {
-	*el_stack.ElStackUdpConn
-}
-
 // ListenELUDP directly creates an el_stack UDP connection and adapts it to discover.UDPConn.
 func ListenELUDP(network string, addr *net.UDPAddr) (discover.UDPConn, error) {
-	c, err := el_stack.NewElStackUdpConn(network, addr)
-	if err != nil {
-		return nil, err
-	}
-	return &ElStackUdpConn{ElStackUdpConn: c}, nil
+	return el_stack.NewElStackUdpConn(network, addr)
 }
