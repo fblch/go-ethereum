4068fec609434446e6fd4abd32431be53b1fabca
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-03-31 16:04:12 +0900

    p2p/elstack: ios/arm64, ios-simulator/arm64, macos/arm64のlibel_stack.aとそれらel_stack.goをリンクする記述を追加してみた(動作未確認)

--- p2p/elstack/el_stack/el_stack.go
+++ p2p/elstack/el_stack/el_stack.go
@@ -1,8 +1,10 @@
 package el_stack
 
 // #cgo CFLAGS: -I${SRCDIR}
-// #cgo !android LDFLAGS: ${SRCDIR}/libs/linux/libel_stack.a -lm
+// #cgo !darwin,ios,arm64 LDFLAGS: ${SRCDIR}/libs/ios_arm64/libel_stack.a -lm
+// #cgo darwin,!ios,arm64 LDFLAGS: ${SRCDIR}/libs/darwin_arm64/libel_stack.a -lm
 // #cgo android,arm64 LDFLAGS: ${SRCDIR}/libs/android_arm64/libel_stack.a -lm
+// #cgo linux,!ios,!darwin,!android LDFLAGS: ${SRCDIR}/libs/linux/libel_stack.a -lm
 // #include <el_stack.h>
 import "C"
 
