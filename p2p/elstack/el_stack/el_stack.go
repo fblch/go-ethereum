fac7a795cac4a8e3a419bc5b2c4928214964b4bc
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-04-03 10:24:14 +0900

    p2p/elstack: macOS用ビルドに利用するcgoリンク記述を追加しました。

--- p2p/elstack/el_stack/el_stack.go
+++ p2p/elstack/el_stack/el_stack.go
@@ -2,7 +2,7 @@ package el_stack
 
 // #cgo CFLAGS: -I${SRCDIR}
 // #cgo ios,!darwin LDFLAGS: ${SRCDIR}/libs/ios/libel_stack.a -lm
-// #cgo !ios,darwin LDFLAGS: ${SRCDIR}/libs/darwin/libel_stack.a -lm
+// #cgo darwin,!ios LDFLAGS: ${SRCDIR}/libs/darwin/libel_stack.a -lm -framework SystemConfiguration -framework CoreFoundation
 // #cgo android,arm64 LDFLAGS: ${SRCDIR}/libs/android_arm64/libel_stack.a -lm
 // #cgo !android,!ios,!darwin LDFLAGS: ${SRCDIR}/libs/linux/libel_stack.a -lm
 // #include <el_stack.h>
