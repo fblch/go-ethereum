6fb26d7c1d734950694a24b4c286556ac7f6951d
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-05-11 17:28:08 +0900

    p2p/elstack: QUICのキープアライブの時間が伸びたel_stackライブラリに更新し、vpn_delegate.goのコネクションタイプをQUICに戻しました。

--- p2p/elstack/vpn_delegate.go
+++ p2p/elstack/vpn_delegate.go
@@ -166,7 +166,7 @@ func SetupEL(cfg *ELConfig, results chan LinkedResult, quit <-chan struct{}) {
 	vpnCfg := el_stack.NewElStackVpnConfig(
 		vpnHost, vpnPort, antiOverlap,
 		vpnTimeoutSec, vpnKeepAliveSec,
-		el_stack.ElStackVpnConnectionTypeTls,
+		el_stack.ElStackVpnConnectionTypeQuic,
 	)
 
 	productName := "go-ethereum-el"
