17eee6b1a908897cd1eefb8d8b00ceaa52ac8fd4
Jakub Pajek <49676311+jakubpajek@users.noreply.github.com>
2026-06-18 12:11:52 +0900

    p2p/elstack: reviewed EL code around logger

--- p2p/elstack/logger.go
+++ p2p/elstack/logger.go
@@ -3,4 +3,4 @@ package elstack
 import "github.com/ethereum/go-ethereum/log"
 
 // Shared logger for elstack package.
-var elLog = log.Root().New("cmp", "p2p/el_stack")
+var elLog = log.New("cmp", "p2p/el_stack")
