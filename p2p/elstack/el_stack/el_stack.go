4b4c868550b2b2f1f04131fe9821f6e43e864938
Hinata-AWAIISHIMA <h.awaiishima@freebit.net>
2026-06-22 15:04:15 +0900

    p2p/elstack/el_stack: linux系のrustライブラリファイルをv1.15.0に更新し、cgoリンク記述を更新しました。

--- p2p/elstack/el_stack/el_stack.go
+++ p2p/elstack/el_stack/el_stack.go
@@ -6,8 +6,8 @@ package el_stack
 // #cgo iossimulator,arm64 LDFLAGS: ${SRCDIR}/libs/iossimulator_arm64/libel_stack_v1.14.23.a -lm
 // #cgo darwin,arm64 LDFLAGS: ${SRCDIR}/libs/darwin_arm64/libel_stack_v1.14.23.a -lm -framework SystemConfiguration -framework CoreFoundation
 // #cgo android,arm64 LDFLAGS: ${SRCDIR}/libs/android_arm64/libel_stack_v1.15.0.a -lm
-// #cgo !android,linux,amd64 LDFLAGS: ${SRCDIR}/libs/linux_amd64/libel_stack_v1.14.23.a -lm
-// #cgo !android,linux,arm64 LDFLAGS: ${SRCDIR}/libs/linux_arm64/libel_stack_v1.14.23.a -lm
+// #cgo !android,linux,amd64 LDFLAGS: ${SRCDIR}/libs/linux_amd64/libel_stack_v1.15.0.a -lm
+// #cgo !android,linux,arm64 LDFLAGS: ${SRCDIR}/libs/linux_arm64/libel_stack_v1.15.0.a -lm
 // #include <el_stack.h>
 import "C"
 
